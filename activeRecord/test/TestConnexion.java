6c39b38c0f1d3c9179ea00019da35e2fce1a901c
EliasKho <elias.khodjaoui@free.fr>
2024-11-19 18:00:08 +0100

    classe de test

--- activeRecord/test/TestConnexion.java
+++ activeRecord/test/TestConnexion.java
@@ -1,3 +1,18 @@
+import activeRecord.DBConnection;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestConnexion {
-    
+    @Test
+    public void testConnexion() throws SQLException {
+        Connection c1 = DBConnection.getConnection();
+        Connection c2 = DBConnection.getConnection();
+        assertEquals("Devraient etre la meme",c1,c2);
+        assertEquals("devrait etre une connection","java.sql.Connection",c2.getClass());
+
+    }
 }
