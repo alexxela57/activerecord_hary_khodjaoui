1d18f54bff191a847de2e052acfec863c69fe2ae
alexxela57 <alexandre.hary3@etu.univ-lorraine.fr>
2024-12-13 22:04:43 +0100

    ajout des nouveaux tests

--- activeRecord/test/TestPersonne.java
+++ activeRecord/test/TestPersonne.java
@@ -1,5 +1,5 @@
-import activeRecord.DBConnection;
-import activeRecord.Personne;
+package activeRecord;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,55 +9,142 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+/**
+ * classe de test
+ */
 public class TestPersonne {
 
-    Personne p1 = new Personne("Spielberg","Steven",1);
-    Personne p2 = new Personne("Scott","Ridley",2);
-    Personne p3 = new Personne("Kubrick","Stanley",3);
-    Personne p4 = new Personne("Fincher","David",4);
-
     @BeforeEach
-    void ajouter() throws SQLException {
+    /**
+     * prepare la base de donnees
+     */
+    public void creerDonnees() throws SQLException {
+        // lien vers la base de test
         Personne.createTable();
+
+        // ajoute
+        new Personne("Spielberg", "Steven").save(); // id 1
+        new Personne("Scott", "Ridley").save(); // id 2
+        new Personne("Kubrick", "Stanley").save(); // id 3
+        new Personne("Spielberg", "George").save(); // id 4
+
     }
 
+    /**
+     * permet de supprimer les donnees � la fin de chaque test
+     *
+     * @throws SQLException
+     */
     @AfterEach
-    void supprimer() throws SQLException {
+    public void supprimerDonnees() throws SQLException {
         Personne.deleteTable();
     }
 
     @Test
+    /**
+     * test constructeur Personne
+     */
+    public void testConstruct() {
+        Personne p = new Personne("toto", "tot");
+        assertEquals(p.getId(), -1,"objet pas dans la base");
+    }
+
+    @Test
+    /**
+     * test find avec un nom existant
+     * @throws SQLException
+     */
+    public void testFindByNameExiste() throws SQLException {
+        ArrayList<Personne> p = Personne.findByName("Kubrick");
+
+        assertEquals(1, p.size(),"une seule reponse");
+        Personne pers = p.get(0);
+        assertEquals(pers.getNom(), "Kubrick");
+        assertEquals(pers.getPrenom(), "Stanley");
+    }
+
+    @Test
+    /**
+     * test find avec un nom dans deux personnes
+     * @throws SQLException
+     */
+    public void testFindByName2Existe() throws SQLException {
+        ArrayList<Personne> p = Personne.findByName("Spielberg");
+
+        assertEquals(2, p.size(),"deux reponses dans personne");
+        Personne pers = p.get(0);
+        assertEquals(pers.getNom(), "Spielberg");
+        pers = p.get(1);
+        assertEquals(pers.getNom(), "Spielberg");
+    }
+
+    @Test
+    /**
+     * test findName avec personne n ayant le nom
+     * @throws SQLException
+     */
+    public void testFindByNameNon() throws SQLException {
+        ArrayList<Personne> p = Personne.findByName("ee");
+        assertEquals(0, p.size(),"pas de reponse");
+    }
+
+    @Test
+    /**
+     * test findall de Personne
+     * @throws SQLException
+     */
     public void testFindAll() throws SQLException {
-        ArrayList<Personne> list = Personne.findAll();
-        assertEquals(true,list.get(0).equals(p1),"devraient etre la meme personne");
-        assertEquals(true,list.get(1).equals(p2),"devraient etre la meme personne");
-        assertEquals(true,list.get(2).equals(p3),"devraient etre la meme personne");
-        assertEquals(true,list.get(3).equals(p4),"devraient etre la meme personne");
+        ArrayList<Personne> p = Personne.findAll();
+        assertEquals(4, p.size());
     }
 
     @Test
-    public void testFindById() throws SQLException {
-        Personne p = Personne.findById(1);
-        assertEquals(true,p.equals(p1),"devraient etre la meme personne");
-    }
+    /**
+     * on test sauvegarde d'une nouvelle personne inexistante
+     */
+    public void testSaveNew() throws SQLException {
+        Personne p = new Personne("toto", "titi");
+        p.save();
 
+        assertEquals(5, p.getId(),"id de la personne vient autoincrement");
+        Personne pers = Personne.findById(5);
+        assertEquals(pers.getNom(), "toto");
+        assertEquals(pers.getPrenom(), "titi");
+        assertEquals(pers.getId(), 5);
+    }
 
     @Test
-    public void testFindByName() throws SQLException {
-        ArrayList<Personne> listPBase = Personne.findByName("Spielberg");
-        ArrayList<Personne> listTest = new ArrayList<Personne>();
-        listTest.add(p1);
+    /**
+     * on test mise a jour d'une personne deja dans la table
+     */
+    public void testSaveExistant() throws SQLException {
+        // modifie la personne
+        Personne p = Personne.findById(2);
+        p.setNom("Bertrand");
+        p.save();
+        assertEquals(2, p.getId(),"id ne devrait pas bouger");
 
-        //comparer la taille des listes
-        assertEquals(listTest.size(), listTest.size(),"Les listes doivent être de la même taille");
-
-        for (int i=0; i<listTest.size(); i++){
-            Personne pres = listTest.get(i);
-            Personne pp = listPBase.get(i);
-
-
-            assertEquals(true, pres.equals(pp),"les personnes devraient être les memes.");
-        }
-
+        // on la recherche
+        ArrayList<Personne> pers = Personne.findByName("Bertrand");
+        assertEquals(1, pers.size());
+        Personne p2 = pers.get(0);
+        assertEquals("Bertrand", p2.getNom());
+        assertEquals("Ridley", p2.getPrenom());
+        assertEquals(p2.getId(), 2,"ide devrait etre le meme");
     }
+
+    @Test
+    /**
+     * on test la suppression d'une personne existante
+     */
+    public void testDelete() throws SQLException {
+        Personne p = Personne.findById(2);
+        p.delete();
+        Personne p2 = Personne.findById(2);
+
+        assertEquals( -1, p.getId(),"id devrait etre revenue a -1");
+        assertEquals( null, p2,"le supprime n'existe plus");
+    }
+
 }
